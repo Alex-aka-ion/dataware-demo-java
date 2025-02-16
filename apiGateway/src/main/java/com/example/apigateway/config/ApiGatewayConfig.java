d4bdd0a356ad5808f3e776f5039e3da2ce9a9323
alexeyyakushin <alex.aka.ion@gmail.com>
2025-02-16 21:48:02 +0400

    добавил сервис api-gateway

---
+++ apiGateway/src/main/java/com/example/apigateway/config/ApiGatewayConfig.java
@@ -0,0 +1,20 @@
+package com.example.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("product-service", r -> r.path("/api/products/**")
+                        .uri("http://localhost:8081"))
+                .route("order-service", r -> r.path("/api/orders/**")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+}
